1b34c3e735f30de4ba09e7fb809ce0d3c2b23888
Vympel87 <111189503+Vympel87@users.noreply.github.com>
2023-04-10 09:45:13 +0700

    Add files via upload

---
+++ Ksatria.java
@@ -0,0 +1,14 @@
+public class Ksatria extends Penduduk{
+
+    public Ksatria(String nama, int pendapatan) {
+        super(nama, pendapatan);
+    }
+
+    public void pajak(int pendapatan, 0.18){
+
+    }
+
+    public void bayar(int pendapatan, int pajak()){
+
+    }
+}
