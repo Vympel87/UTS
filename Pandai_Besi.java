393fa7851642a5623596212bd64047801fe2c2a3
Vympel87 <111189503+Vympel87@users.noreply.github.com>
2023-04-10 09:45:35 +0700

    Add files via upload

---
+++ Pandai_Besi.java
@@ -0,0 +1,11 @@
+public class Pandai_Besi extends Penduduk{
+    public Pandai_Besi(String nama, int pendapatan) {
+        super(nama, pendapatan);
+    }
+
+    public void pajak(int pendapatan, 0.06){
+
+    }
+
+    public void bayar(int pendapatan, int pajak())
+}
