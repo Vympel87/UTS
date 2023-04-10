bcc561f46c61b6d363954150737c2f1eb5b74c43
Vympel87 <111189503+Vympel87@users.noreply.github.com>
2023-04-10 09:46:27 +0700

    Add files via upload

---
+++ Petani.java
@@ -0,0 +1,13 @@
+public class Petani extends Penduduk{
+    public Petani(String nama, int pendapatan) {
+        super(nama, pendapatan);
+    }
+
+    public void pajak(int pendapatan, 0.03){
+
+    }
+
+    public void bayar(int pendapatan, int pajak()){
+
+    }
+}
