a679167a9623afdca9ba5b5e1b30163117e798f2
Vympel87 <111189503+Vympel87@users.noreply.github.com>
2023-04-10 09:47:14 +0700

    Add files via upload

---
+++ Pedagang.java
@@ -0,0 +1,13 @@
+public class Pedagang extends Penduduk{
+    public Pedagang(String nama, int pendapatan) {
+        super(nama, pendapatan);
+    }
+    public void pajak(int pendapatan, 0.12){
+
+    }
+
+    public void bayar(int pendapatan, int pajak()){
+
+    }
+
+}
