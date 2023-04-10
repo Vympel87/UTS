eb992dccd62348e75cdda9e7aee5d7ea193c1803
Vympel87 <111189503+Vympel87@users.noreply.github.com>
2023-04-10 09:46:47 +0700

    Add files via upload

---
+++ Penduduk.java
@@ -0,0 +1,13 @@
+public class Penduduk {
+    String nama;
+    int pendapatan;
+
+    public Penduduk(String nama, int pendapatan) {
+        this.nama = nama;
+        this.pendapatan = pendapatan;
+    }
+
+    public void Penduduk(String nama, int pendapatan){
+
+    }
+}
